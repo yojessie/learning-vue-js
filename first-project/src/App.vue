8e13068b16b13eb97a6a3f0d3f2a142b21ca141d
yojessie <sprout.jessie@gmail.com>
2022-06-28 09:59:42 +0900

    🚧  Study data binding

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -1,22 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h4 :style="style1">{{ products[0] }}</h4>
+    <p>{{ prices[0] }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ prices[1] }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ prices[2] }}만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      // 자주 바뀌는 데이터는 따로 담아서 데이터바인딩
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      prices: [50, 60, 100],
+      // HTML 속성도 데이터바인딩이 가능하다. HTML태그 안에 사용 시 앞에 콜론 붙일 것
+      style1: "color: tomato",
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
