274eb38e29e2acebd4c49f09de3bb282ceb4bdbf
jessie-yook <sprout.jessie@gmail.com>
2022-07-01 11:21:14 +0900

    ♻️  Implement the bottom section by using loop

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -6,32 +6,37 @@
     <!-- 변수는 두개까지 만들 수 있고, 오른쪽 변수는 1씩 증가하는 정수이다. 오른쪽 변수를 key값으로 활용한다. -->
   </div>
 
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>{{ prices[0] }}만원</p>
-    <button @click="buttonCount[0]++">허위매물신고</button>
-    <!-- 이벤트리스너 사용하기 : v-on:click=" " 혹은 @click=" " -->
-    <span>신고 수 : {{ buttonCount[0] }}</span>
-  </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ prices[1] }}만원</p>
-    <button @click="buttonCount[1]++">허위매물신고</button>
-    <span>신고 수 : {{ buttonCount[1] }}</span>
-  </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p>{{ prices[2] }}만원</p>
-    <button @click="buttonCount[2]++">허위매물신고</button>
-    <span>신고 수 : {{ buttonCount[2] }}</span>
-  </div>
+  <section class="section">
+    <div>
+      <h3>{{ products[0] }}</h3>
+      <p>{{ prices[0] }}만원</p>
+      <button @click="buttonCount[0]++">허위매물신고</button>
+      <!-- 이벤트리스너 사용하기 : v-on:click=" " 혹은 @click=" " -->
+      <span>신고 수 : {{ buttonCount[0] }}</span>
+    </div>
+    <div>
+      <h3>{{ products[1] }}</h3>
+      <p>{{ prices[1] }}만원</p>
+      <button @click="buttonCount[1]++">허위매물신고</button>
+      <span>신고 수 : {{ buttonCount[1] }}</span>
+    </div>
+    <div>
+      <h3>{{ products[2] }}</h3>
+      <p>{{ prices[2] }}만원</p>
+      <button @click="buttonCount[2]++">허위매물신고</button>
+      <span>신고 수 : {{ buttonCount[2] }}</span>
+    </div>
+  </section>
 
-  <h2 :style="style1">반복문 사용해서 다시 만들기</h2>
-  <div v-for="(product, i) in products" :key="i">
-    <h4>{{ product }}</h4>
-    <p>{{ prices[0] }}만원</p>
-    <!-- 반복문을 한번 돌릴때 데이터 2가지를 어떻게 가져오는걸까? -->
-  </div>
+  <section class="section">
+    <h4 :style="style1">위 내용 반복문 사용해서 다시 만들기</h4>
+    <div v-for="(product, i) in productList" :key="i">
+      <h3>{{ product.name }}</h3>
+      <p>{{ product.price }}만원</p>
+      <button @click="product.buttonCount++">허위매물신고</button>
+      <span>신고 수 : {{ product.buttonCount }}</span>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -46,6 +51,12 @@ export default {
       // HTML 속성도 데이터바인딩이 가능하다. HTML태그 안에 사용 시 앞에 콜론 붙일 것
       style1: "color: tomato",
       buttonCount: [0, 0, 0],
+      // 반복문용 데이터
+      productList: [
+        { name: "역삼동원룸", price: 50, buttonCount: 0 },
+        { name: "천호동원룸", price: 60, buttonCount: 0 },
+        { name: "자양동원룸", price: 100, buttonCount: 0 },
+      ],
     };
   },
   methods: {
@@ -65,6 +76,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 40px;
+}
+
+button {
+  margin-right: 4px;
 }
 
 .navigation {
@@ -79,5 +95,12 @@ export default {
   font-weight: 700;
   color: white;
   padding: 16px 8px;
+}
+
+.section {
+  padding: 40px;
+  border-radius: 8px;
+  background-color: #f2f2f2;
+  margin-bottom: 40px;
 }
 </style>
